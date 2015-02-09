6141303a7a4afc96d9e0613e70a197d78e5009b9
Blake Embrey <hello@blakeembrey.com>
2015-02-09 21:55:23 +0900

    Don't use spaces in regexp
    Fixes #2

--- index.js
+++ index.js
@@ -16,12 +16,10 @@
 module.exports =  function metntionsRegex(options) {
   options = options || {};
 
-  var startSpace = options.startSpace === false ? '' : '\\s+';
-  var endSpace = options.endSpace === false ? '' : '\\s+';
   var length = '{1,' + (options.length || 30) + '}';
   var match = options.match || '\\w' + length;
 
   match = options.dot && !options.match ? '[A-Za-z0-9_.]' + length : match
 
-  return new RegExp(startSpace + '@(' + match + ')' + endSpace, options.flags);
+  return new RegExp('(?!\\b)@(' + match + ')\\b', options.flags);
 };
